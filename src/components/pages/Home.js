6f9580623270abc591ae5b3c14effec0228c7433
Akash kumar <akaysharma894@gmail.com>
2021-09-11 21:21:59 +0530

    Added home

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.css";
+import Card from "../Card";
+import HeroSection from "../HeroSection";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <Card />
+    </>
+  );
+};
+
+export default Home;
