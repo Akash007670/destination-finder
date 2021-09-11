8e972f30c435bcd49079c1c154d68dfcda512328
Akash kumar <akaysharma894@gmail.com>
2021-09-11 21:22:24 +0530

    Added images and video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navabar from "./components/Navabar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Button from "./components/Button";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navabar />
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home} />
         </Switch>
         <Button />
       </Router>
