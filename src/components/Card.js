c7be4bcea2bd92bfb59455f263a575ca9e0457db
Akash kumar <akaysharma894@gmail.com>
2021-09-17 23:59:07 +0530

    Added images container

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,8 +10,34 @@ const Card = () => {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="./assets/images/img-9.jpg"
+              src={process.env.PUBLIC_URL + "/images/img-9.jpg"}
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={process.env.PUBLIC_URL + "/images/img-7.jpg"}
+              text="Explore the Hill station and amazing skiing view"
+              label="Adventure"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src={process.env.PUBLIC_URL + "/images/img-1.jpg"}
+              text="Explore the amazing sunrise at himalayas"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={process.env.PUBLIC_URL + "/images/img-2.jpg"}
+              text="Explore the Explore the amazing view of bali"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={process.env.PUBLIC_URL + "/images/img-8.jpg"}
+              text="Explore the amazing sahara desert in india"
               label="Adventure"
               path="/services"
             />
