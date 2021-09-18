da710ef4a0d9a206bc07164c161edce6f76ebb79
Akash kumar <akaysharma894@gmail.com>
2021-09-18 23:39:05 +0530

    Added route path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navabar from "./components/Navabar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Button from "./components/Button";
 import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Product from "./components/pages/Product";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navabar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Product} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
         <Button />
       </Router>
